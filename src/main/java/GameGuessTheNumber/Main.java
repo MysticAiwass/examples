c0f24b3788ee14addeb67312dfaff178a996e98f
EvaAiwass <evawhite400@gmail.com>
2023-10-13 12:54:35 +0700

    Добавила программу DefaultGameGuessTheNumber

---
+++ src/main/java/GameGuessTheNumber/Main.java
@@ -0,0 +1,10 @@
+package GameGuessTheNumber;
+
+public class Main {
+    public static void main(String[] args) {
+        DefaultGameGuessTheNumber doSomething = new DefaultGameGuessTheNumber();
+
+        doSomething.startGame();
+    }
+}
+
